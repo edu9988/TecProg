3385d68f823081b137f261b06dcb1a4c373252ac
Jose Eduardo <jose.peres@usp.br>
2019-09-11 17:38:44 -0300

    Cria o registro global_params
    Para fins de organização, armazena as variáveis anteriormente "soltas" em uma typedef struct global_params.

--- fase1.c
+++ fase1.c
@@ -23,9 +23,18 @@ typedef struct{
     double vel_y;
 }corpo;
 
-void read_entry_file(nave *s1, nave *s2, corpo **corpos);
+typedef struct{
+    double planet_mass;
+    double planet_radius;
+    double total_time;
+    int projectiles_quantity;
+    double projectiles_lifespan;
+}global_params;
+
+void read_entry_file(global_params *p0, nave *s1, nave *s2, corpo **corpos);
 void *mallocSafe(int nbytes);
 void string_copy(char *a, char *b);
+void next_pos(global_params *p0, nave *s1, nave *s2, corpo **corpos);
 
 int main(int argc, char *argv[]){
     double delta_t;
@@ -42,9 +51,10 @@ int main(int argc, char *argv[]){
     nave ship1;
     nave ship2;
     corpo *body_list;
-    body_list = NULL;
-    read_entry_file( &ship1 , &ship2 , &body_list );
+    global_params parametros;
+    read_entry_file( &parametros , &ship1 , &ship2 , &body_list );
 
+    /*	free's section	*/
     free( ship1.name );
     ship1.name = NULL;
     free( ship2.name );
@@ -54,10 +64,8 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-void read_entry_file(nave *s1, nave *s2, corpo **corpos){
+void read_entry_file(global_params *p0, nave *s1, nave *s2, corpo **corpos){
     char aux_name[30];
-    int aux_int;
-    double aux_double;
     FILE *arq;
     arq = fopen("entry.dat", "r");
     if( !arq ){
@@ -65,12 +73,9 @@ void read_entry_file(nave *s1, nave *s2, corpo **corpos){
 	exit(EXIT_FAILURE);
     }
     /*	Planet		*/
-    fscanf(arq, "%lf", &aux_double);
-    printf("%lf ", aux_double);
-    fscanf(arq, "%lf", &aux_double);
-    printf("%lf ", aux_double);
-    fscanf(arq, "%lf", &aux_double);
-    printf("%lf \n", aux_double);
+    fscanf(arq, "%lf", &(p0->planet_mass));
+    fscanf(arq, "%lf", &(p0->planet_radius));
+    fscanf(arq, "%lf", &(p0->total_time));
     /*	Spacecraft 1	*/
     fscanf(arq, " %s", aux_name);
     s1->name = mallocSafe( (1+strlen(aux_name))*sizeof(char) );
@@ -90,12 +95,12 @@ void read_entry_file(nave *s1, nave *s2, corpo **corpos){
     fscanf(arq, "%lf", &(s2->vel_x));
     fscanf(arq, "%lf", &(s2->vel_y));
     /*	Projectiles	*/
-    fscanf(arq, "%d", &aux_int);
-    *corpos = mallocSafe( aux_int*sizeof(corpo) );
-    printf("%d ", aux_int);
-    fscanf(arq, "%lf", &aux_double);
-    printf("%lf\n", aux_double);
-    for( int i=0; i<aux_int ; i++ ){
+    fscanf(arq, "%d", &(p0->projectiles_quantity));
+    *corpos = mallocSafe( p0->projectiles_quantity*sizeof(corpo) );
+    printf("%d ", p0->projectiles_quantity);
+    fscanf(arq, "%lf", &(p0->projectiles_lifespan));
+    printf("%lf\n", p0->projectiles_lifespan);
+    for( int i=0; i<p0->projectiles_quantity ; i++ ){
 	fscanf(arq, "%lf", &((*corpos)[i].mass));
 	fscanf(arq, "%lf", &((*corpos)[i].pos_x));
 	fscanf(arq, "%lf", &((*corpos)[i].pos_y));
@@ -124,4 +129,6 @@ void string_copy(char *a, char *b){
     return;
 }
 
-
+void next_pos(global_params *p0, nave *s1, nave *s2, corpo **corpos){
+    return;  /*to be done*/
+}
