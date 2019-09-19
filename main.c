47928c83df17a7771c4eb7fdc9be10921836548c
marcelojunior1 <55034249+marcelojunior1@users.noreply.github.com>
2019-09-19 00:29:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <math.h>
 
+#define G 6.67e-11
+
 double tempoSimulacao;
 double massaPlaneta;
 double raioPlaneta;
@@ -16,11 +18,11 @@ typedef struct{
 typedef struct {
     vetor velObj;
     vetor posObj;
+    double massa;
 }objeto;
 
 typedef struct {
     char *nome;
-    double massa;
     objeto valor;
 }nave;
 
@@ -29,7 +31,6 @@ typedef struct {
 }projetil;
 
 typedef struct {
-    double massa;
     double raio;
     objeto valor;
 }planeta;
@@ -37,12 +38,13 @@ typedef struct {
 void draw(nave *nave1, nave *nave2, planeta planeta1);
 void addVetorAB(vetor *a, vetor b);
 double normaVetor(vetor A);
-void versorVetor(vetor *A);
+void versorVetor(vetor *rVetor, vetor A, vetor B);
 void printVetor(vetor A);
 void distanciaAB(vetor *V, vetor A, vetor B);
 void carregarArquivos(nave *nave1, nave *nave2, planeta *planeta, projetil *projetilX);
 void *mallocSafe(int nbytes);
 void string_copy(char *a, char *b);
+void aceleracao(objeto *A, objeto B);
 
 int main(int argc, char *argv[]){
     nave nave1;
@@ -51,15 +53,17 @@ int main(int argc, char *argv[]){
     vetor acelAux;
     planeta planeta1;
     projetil projetil1X;
-
     int i;
 
     planeta1.valor.posObj.x = 0;
     planeta1.valor.posObj.y = 0;
 
-
     carregarArquivos(&nave1, &nave2, &planeta1, &projetil1X);
 
+    nave1.valor.posObj.x = 6000000;
+    nave1.valor.posObj.y = 6000000;
+    nave1.valor.velObj.x = 0;
+    nave1.valor.velObj.y = 0;
 
     for(i=0; i<1000; i++)
         draw(&nave1, &nave2, planeta1);
@@ -69,12 +73,15 @@ int main(int argc, char *argv[]){
 
 void draw(nave *nave1, nave *nave2, planeta planeta1)
 {
+    aceleracao(&nave1->valor, planeta1.valor);
 
     addVetorAB(&nave1->valor.posObj, nave1->valor.velObj);
-    addVetorAB(&nave2->valor.posObj, nave2->valor.velObj);
+    //printVetor(nave1->valor.velObj);
+    //addVetorAB(&nave2->valor.velObj, );
+
+
 
     printVetor(nave1->valor.posObj);
-    printVetor(nave2->valor.posObj);
     printf("\n");
 }
 
@@ -87,15 +94,17 @@ double normaVetor(vetor A){
     return (sqrt(A.x * A.x + A.y * A.y));
 }
 
-void versorVetor(vetor *A){
-    double norma = normaVetor(*A);
-    A->x = A->x/norma;
-    A->y = A->y/norma;
+void versorVetor(vetor *rVetor, vetor A, vetor B){
+    double norma = normaVetor(A);
+
+    distanciaAB(rVetor, B, A);
+    rVetor->x = rVetor->x/norma;
+    rVetor->y = rVetor->y/norma;
 }
 
 void printVetor(vetor A)
 {
-    printf("%.2lf\t%.2lf\t", A.x, A.y);
+    printf("%.2lf %.2lf", A.x, A.y);
 }
 
 void distanciaAB(vetor *V, vetor A, vetor B)
@@ -120,18 +129,16 @@ void carregarArquivos(nave *nave1, nave *nave2, planeta *planeta, projetil *proj
 
     //CONVERTER O NUMERO ANTES DE REGISTRAR, FSCANF NÃO MOSTRA ERROS
 
-    fscanf(arquivoDeEntrada, "%le %le %lf", &planeta->raio, &planeta->massa, &tempoSimulacao);
+    fscanf(arquivoDeEntrada, "%le %le %lf", &planeta->raio, &planeta->valor.massa, &tempoSimulacao);
 
-    fscanf(arquivoDeEntrada, "%s %lf %lf %lf %lf %lf", auxiliar, &nave1->massa, &nave1->valor.posObj.x,
+    fscanf(arquivoDeEntrada, "%s %lf %lf %lf %lf %lf", auxiliar, &nave1->valor.massa, &nave1->valor.posObj.x,
             &nave1->valor.posObj.y, &nave1->valor.velObj.x, &nave1->valor.velObj.y);
-
-    printVetor(nave1->valor.posObj);
 
     nave1->nome = mallocSafe((1+strlen(auxiliar))*sizeof(char));
     string_copy(auxiliar , nave1->nome);
 
 
-    fscanf(arquivoDeEntrada, "%s %lf %lf %lf %lf %lf", auxiliar, &nave2->massa,
+    fscanf(arquivoDeEntrada, "%s %lf %lf %lf %lf %lf", auxiliar, &nave2->valor.massa,
            &nave2->valor.posObj.x, &nave2->valor.posObj.y, &nave2->valor.velObj.x, &nave2->valor.velObj.y);
 
     nave2->nome = mallocSafe((1+strlen(auxiliar))*sizeof(char));
@@ -163,4 +170,22 @@ void string_copy(char *a, char *b)
         b[i] = a[i];
     b[i] = '\0';
     return;
+}
+
+void aceleracao(objeto *A, objeto B){
+    vetor rVetor;
+    vetor distancia;
+    versorVetor(&rVetor, A->posObj, B.posObj);
+    distanciaAB(&distancia, A->posObj, B.posObj);
+
+    if(distancia.x == 0 || distancia.y == 0){
+        distancia.y = 10;
+        distancia.x = 10;
+    }
+
+    A->velObj.x += G*(rVetor.x)*(B.massa)/(distancia.x * distancia.x);
+    A->velObj.y += G*(rVetor.y)*(B.massa)/(distancia.y * distancia.y);
+
+    //printVetor(rVetor);
+
 }
