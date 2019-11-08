d21ea34a19b651e4e38804762e1a84361e13f37d
marcelojunior1 <55034249+marcelojunior1@users.noreply.github.com>
2019-11-08 18:01:20 -0300

    Add files via upload
    grafic.c atualizada com a mascara funcionando.

--- grafico.c
+++ grafico.c
@@ -1,12 +1,11 @@
 /**************************************************************/
 /* José Eduardo R. M. Peres y Peres       No. USP:8945332     */
 /* Marcelo Nascimento dos Santos Junior	  No. USP:11222012    */
-/* Gilvane da Silva Sousa		          No. USP:10258726    */
-/*							                                  */
-/* Projeto - Terceira fase - 10 nov 2019                      */
-/* Curso MAC0216  - Prof. Marco Dimas Gubitoso		          */
+/* Gilvane da Silva Sousa		  No. USP:10258726    */
+/*							      */
+/* Projeto - Primeira fase - 22 set 2019                      */
+/* Curso MAC0216  - Prof. Marco Dimas Gubitoso		      */
 /**************************************************************/
-
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,144 +15,193 @@
 #include "xwc.h"
 #include "grafico.h"
 
-#define PI 3.14159265359
+/* XPM */
+/* figura qualquer para tentar implementar transparencia*/
+static char *samp_xpm[] = {
+/* nose pixmap
+ width height ncolors chars_per_pixel */
+"32 32 5 1",
+" 	c None",
+".	c blue",
+"X	c white",
+"o	c gold",
+"O	c yellow",
+/* pixels */
+"           ..XXX..              ",
+"        ..XXXoXXXXX..           ",
+"       .XooOOOooooXXX.          ",
+"      XooOOOOOOOooooXXX         ",
+"     XoOOOOOOOOOOOoooXXX        ",
+"    .ooOOOOOOOOOOOooooXX.       ",
+"   .XoOOOOOOOOOOOOOoooXXX.      ",
+"  ..ooOOOOOOOOOOOOOooooXX..     ",
+"  .XoOOOOOOOOOOOOOOOoooXXX.     ",
+"  .XoOOOOOOOOOOOOOOOoooXXX.     ",
+" ..XoOOOOOOOOOOOOOOOoooXXX..    ",
+" .XoooOOOOOOOOOOOOOoooooXXX.    ",
+" .XXooOOOOOOOOOOOOOooooXXXX.    ",
+" .XXoooOOOOOOOOOOOoooooXXXX.    ",
+" ..XoooOOOOOOOOOOOoooooXXX..    ",
+" ..XoooooOOOOOOOoooooooXXX..    ",
+" ..XXooooooOOOooooooooXXXX..    ",
+"  ..XoooooooooooooooooXXX..     ",
+"  ..XXoooooooooooooooXXXX..     ",
+"  ...XXoooooooooooooXXXX...     ",
+"   ..XXXXoooooooooXXXXXX..      ",
+"    ..XXXXXXXoXXXXXXXXX..       ",
+"     ...XXXXXXXXXXXXX...        ",
+"      ....XXXXXXXXX....         ",
+"         .....X.....            ",
+"          .........             ",
+"           .......              ",
+"                                ",
+"                                ",
+"                                ",
+"                                ",
+"                                "
+};
 
-extern constants p0;
-extern corpo *body_list;
-
-double posX=100, posY=100;
-double velX = 0, velY = 0;
+static int Tx(double);
+static int Ty(double);
+static int sprite_x();
 
 extern WINDOW *w;
-static PIC Ms, Ms2, pFigura2, pFigura1, pFigura3, pBackup;
-static Color misseis, teste;
-static MASK msk1, msk2, msk3;
+extern constants p0;
+extern corpo *body_list;
+tela t0;
+static PIC P1, P2, Ms, Aux, fundo1, fundo2, planeta;
+static Color player1, misseis;
+static int indice;
+MASK masc1, masc2, masc_missiles, masc_planet, mascAux; /*ADICIONEI UMA MASCARA AUXILIAR*/
+static int Be(double p);
+static void GeraFundo();
+static char palavra[30];
 
-extern int k;
-
-int rotacaoX = 0, rotacaoY = 0;
-double angulo = 0;
-double constVel = 2;
-
-int aux1;
-
-static int i,j;
-char aux[30], aux2[30];
-
-KeySym key;
-
+/*
+init_modulo_grafico():
+Aloca as estruturas e inicializa as variaveis necessárias 
+*/
 void init_modulo_grafico(){
-
-    w = InitGraph( p0.SCR_larg, p0.SCR_alt, "Fase 2");
-    pFigura1 = NewPic(w, 64, 64);
-    pFigura2 = NewPic(w, 70, 70);
-    pFigura3= NewPic(w, 70, 70);
-
-
-    pBackup = NewPic(w, p0.SCR_larg,p0.SCR_alt);
-
-    msk1 = NewMask(w, 64, 64);
-    msk2 = NewMask(w, 70, 70);
-    msk3 = NewMask(w, 70, 70);
-
-    Ms = NewPic( w , 10 , 10);
-    Ms2 = NewPic(w, 10, 10);
-
-
-
-    pFigura1 = ReadPic(w, "0.xpm", msk1);
-    /*pFigura2 = ReadPic(w, "seta.xpm", msk2);*/
-
-
-    /*XWriteBitmapFile(GetDisplay(), "seta.dat", GetDraw(pFigura2), 50, 50, 0, 0);*/
-
+    indice = 0;
+    w = InitGraph( t0.SCR_larg, t0.SCR_alt, "SpaceWar");
+    Aux = NewPic( w , t0.SCR_larg,t0.SCR_alt );
+    masc1 = NewMask( w , 50,50 );
+    masc2 = NewMask( w , 50,50 );
+    mascAux = NewMask( w , 1200, 50 ); /*INICIALIZEI COM O TAMANHO DA IMAGEM ORIGINAL*/
+    masc_missiles = NewMask( w , 50 , 50 );
+    masc_planet = NewMask( w , t0.SCR_larg , t0.SCR_alt );
+    fundo1 = NewPic( w , t0.SCR_larg,t0.SCR_alt );
+    fundo2 = NewPic( w , t0.SCR_larg,t0.SCR_alt );
+    planeta = ReadPic( w , "planeta1.xpm" , masc_planet );
+    GeraFundo();
+    P1 = ReadPic( w, "spaceshuttle_bw1.xpm", mascAux); /*JOGUEI OS DADOS DA MASCARA DA IMAGEM EM "MASCAUX" */
+    P2 = ReadPic( w, "teste1.xpm" , masc2 );
+    Ms = MountPic( w, samp_xpm , masc_missiles );
+    t0.planeta_w =(int) ( (p0.planet_radius/p0.L) *(double)t0.SCR_larg ) ;
+    t0.planeta_h = t0.planeta_w;
+    t0.planeta_x = t0.SCR_larg/2 - t0.planeta_w/2;
+    t0.planeta_y = t0.SCR_alt/2 - t0.planeta_w/2;
+    player1 = WNamedColor("red");
     misseis = WNamedColor("gold");
-    teste = WNamedColor("white");
-
-    WFillRect(Ms, 0, 0 , 5,5 , misseis);
-    WFillRect(Ms2, 0, 0, 5, 5, teste);
-
     InitKBD(w);
-
-    WFillRect( w , 0,0 , p0.SCR_larg,p0.SCR_alt , 0x000055 );
-
-    PutPic(pBackup, w, 0, 0, p0.SCR_larg, p0.SCR_alt, 0, 0);
-
-    return;
 }
 
+/*
+graficos_iteracao():
+Desenha na tela todos os corpos em suas respectivas
+posicoes.
+*/
 void graficos_iteracao(){
-
     int i, n;
-    double norma;
-
     n = p0.projectiles_quantity;
-
-    if(WCheckKBD(w)){
-        key = WGetKey(w);
-/*********************************************************************/
-
-        if(key == 114)
-            angulo += PI/8;
-        if(key == 113)
-            angulo += -PI/8;
-
-        if(angulo < 0)
-            angulo = 2*PI + angulo;
-
-        if(angulo >= 2*PI || angulo <= -2*PI)
-            angulo = 0;
-
-        PutPic(pFigura2, pFigura3, 0,0,64,64,0,0);
-
-        sprintf(aux2, "%.f", fabs(10*angulo*180/PI));
-
-        pFigura2 = ReadPic(w, strcat(aux2, ".xpm"), msk2);
-
-/****************************************************************************/
-    }
-
     for( i=0 ; i<n+2 ; i++ ){
-        body_list[i].SCR_pos_x = Tx( body_list[i].pos_x );
-        body_list[i].SCR_pos_y = Ty( body_list[i].pos_y );
-
-        if(i>1){
-            body_list[i].SCR_pos_x += 20;
-            body_list[i].SCR_pos_y += 20;
-        }
+	body_list[i].SCR_pos_x = Tx( body_list[i].pos_x );
+	body_list[i].SCR_pos_y = Ty( body_list[i].pos_y );
     }
 
-    norma = sqrt( pow(body_list[0].SCR_pos_x -15 - posX, 2) + pow(body_list[0].SCR_pos_y -15 - posY, 2));
+    /*WFillArc( Aux , t0.planeta_x,t0.planeta_y  , 0,23040 , t0.planeta_w,t0.planeta_h , 0x5050FF );*/
+    SetMask( Aux , masc_planet );
+    PutPic( Aux , planeta ,0,0 , t0.planeta_w,t0.planeta_h , t0.planeta_x,t0.planeta_y );
+    SetMask( Aux , masc2 );
+    PutPic( Aux , P2 ,0,0 , 50,40, body_list[1].SCR_pos_x,body_list[1].SCR_pos_y);
 
-    velX = ((body_list[0].SCR_pos_x - posX)/norma)*0.16;
-    posX += velX;
-    velY = ((body_list[0].SCR_pos_y - posY)/norma)*0.16;
-    posY += velY;
+    PutPic(masc1 , mascAux ,sprite_x(),0 , 50,50, 0,0); /*IMPRIMI SOMENTE O TAMANHO NECESSARIO DA MASCARA ORIGINAL NA MASC1*/
 
-    PutPic(w, pBackup, 0, 0, p0.SCR_larg, p0.SCR_alt, 0, 0);
-
+    SetMask(Aux , masc1 );
+    PutPic( Aux , P1 ,sprite_x(),0 , 50,50, body_list[0].SCR_pos_x-25,body_list[0].SCR_pos_y-25);
+    SetMask( Aux , masc_missiles );
     for( i=0 ; i<n ; i++ )
-        PutPic(w, Ms ,0,0 , 5,5, body_list[i+2].SCR_pos_x,body_list[i+2].SCR_pos_y);
-/**************************************************************************************/
+    	PutPic( Aux , Ms ,0,0 , 40,40, body_list[i+2].SCR_pos_x-5,body_list[i+2].SCR_pos_y-5);
+    UnSetMask( Aux );
+    WCor( Aux , 0xFF0000 );
+    sprintf(palavra, "%d", indice);
+    WPrint( Aux , 20 , 180 , palavra );
+    PutPic( w , Aux ,0,0 , t0.SCR_larg,t0.SCR_alt , 0,0 );
+    WFillRect( masc1 , 0,0 , 50,50 , WNamedColor( "None" ) );
+    /* Refaz fundo */
+    
+    if( indice%2 )
+	PutPic( Aux , fundo1 ,0,0 , t0.SCR_larg,t0.SCR_alt , 0,0 );
+    else
+	PutPic( Aux , fundo2 ,0,0 , t0.SCR_larg,t0.SCR_alt , 0,0 );
+    indice++;
+}
 
-    PutPic(w, Ms, 0, 0, 5, 5, posX, posY);
+static int sprite_x(){
+    double a;
+    int b;
 
+    a = body_list[0].angulo;
+    if( a < 0 ){
+	a *= -57.296;
+	a -= 7.5;
+	b = (int) a / 15;
+	return 50*b;
+    }
+    else{
+	a *= 57.296;
+	a -= 7.5;
+	b = (int) a / 15;
+	return (23-b)*50;
+    }
+}
 
-/**************************************************************************************/
+/*
+GeraFundo():
+Preenche as PICs fundo1 e fundo2 com estrelas.
+*/
+static void GeraFundo(){
+    int x,y;
+    for( y=0 ; y<t0.SCR_alt ; y++ )
+	for( x = 0 ; x<t0.SCR_larg ; x++ )
+	    if( Be( 1.0e-4 ) ){
+		WFillArc( fundo1 , x,y , 0,23040 , 3,3 , 0x5050FF);
+		WPlot( fundo1 , x+1,y+1 , 0x8080FF );
+		WFillArc( fundo2 , x,y , 0,23040 , 3,3 ,  0x0000FF);
+		WPlot( fundo2 , x+1,y+1 , 0xFF00FF );
+	    }
+}
 
-    /*WLine(w, posX, posY, body_list[0].SCR_pos_x+20, body_list[0].SCR_pos_y, teste);*/
+/*
+Be():
+Dado um real p, 0 <= p <= 1, retorna 0 ou 1.
+A probabilidade de retornar 1 é dada por p.
+*/
+/*Funcao fornecida por: Yoshiharu Kohayakawa*/
+static int Be( double p ){
+    return rand()/(1.0 + RAND_MAX) < p;
+}
 
-    SetMask(w, msk1);
-    PutPic(w, pFigura1, 0, 0, 64, 64, body_list[0].SCR_pos_x, body_list[0].SCR_pos_y);
-
-    SetMask(w, msk2);
-    PutPic(w, pFigura2, 0, 0, 70, 70, posX-30, posY-30);
-    UnSetMask(w);
-
-    sprintf(aux, "%d", k);
-    WPrint(w, 20, 200, aux);
-
-    return;
+/*
+termina_modulo_grafico():
+Libera as estruturas alocadas
+*/
+void termina_modulo_grafico(){
+    FreePic( P1 );
+    FreePic( P2 );
+    FreePic( Ms );
+    WDestroy( w );
+    CloseGraph();
 }
 
 /*
@@ -163,11 +211,11 @@ Calcula a posicao na tela, Tx, atraves de uma
 transformacao de variaveis.
 Tx pertence ao intervalo [0,SCR_larg];
 */
-int Tx(double pos_x){
+static int Tx(double pos_x){
     int transf_x;
     double tx;
     tx = 1 + pos_x/p0.L;
-    tx *= p0.SCR_larg/2;
+    tx *= t0.SCR_larg/2;
     transf_x = (int) tx;
     return transf_x;
 }
@@ -179,11 +227,11 @@ Calcula a posicao na tela, Ty, atraves de uma
 transformacao de variaveis.
 Ty pertence ao intervalo [0,SCR_alt];
 */
-int Ty(double pos_y){
+static int Ty(double pos_y){
     int transf_y;
     double ty;
     ty = 1 - pos_y/p0.H;
-    ty *= p0.SCR_alt/2;
+    ty *= t0.SCR_alt/2;
     transf_y = (int) ty;
     return transf_y;
 }
